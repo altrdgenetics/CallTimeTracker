7465e49d50b970aa1b394e5258e551e90334a032
altrdgenetics <tofudeliveryae86@hotmail.com>
2017-06-05 15:54:19 -0400

    Added in base statements for the all button

--- src/main/java/altrdgenetics/callTimeTracker/sceneController/MainWindowSceneController.java
+++ src/main/java/altrdgenetics/callTimeTracker/sceneController/MainWindowSceneController.java
@@ -1,5 +1,6 @@
 package altrdgenetics.callTimeTracker.sceneController;
 
+import altrdgenetics.callTimeTracker.util.StringUtilities;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -7,13 +8,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 
-public class FXMLController implements Initializable {
+public class MainWindowSceneController implements Initializable {
     
     @FXML
     private Button RecordButton;
     @FXML
     private ComboBox CompanyComboBox;
+    @FXML
+    private Label TimerLabel;
+    
+    private long callStartTime;
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -34,11 +40,19 @@ public class FXMLController implements Initializable {
     }
     
     private void startCall() {
+        callStartTime = System.currentTimeMillis();    
+        TimerLabel.setText("");
         
+        RecordButton.setText("End Call");
     }
     
     private void endCall() {
+        long callEndTime = System.currentTimeMillis();
+        TimerLabel.setText(StringUtilities.convertLongToTime(callEndTime - callStartTime));
         
+        //Record call To Database
+        
+        RecordButton.setText("Start Call");
     }
     
     
